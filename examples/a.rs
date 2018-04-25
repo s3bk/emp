804ae8cbc25d92ab62775bcd757f39f0fd906db5
Sebastian Köln <sebk@rynx.org>
2018-04-25 15:14:11 +0200

    .

--- examples/a.rs
+++ examples/a.rs
@@ -12,37 +12,6 @@ struct Bar(u32);
 #[derive(Debug)]
 struct Baz(String);
 
-
-/*
-fn spawn_test() -> Process {
-    let mut any = 0;
-    let mut bar = 0;
-    
-    Process::new(move || {
-        loop {
-            const FOO_ID: TypeId = TypeId::of::<Foo>();
-            const BAR_ID: TypeId = TypeId::of::<Bar>();
-            while let Some(e) = self.inbox.get() {
-                match e.type_id {
-                    FOO_ID => {
-                        let _ = *downcast::<Foo>(e);
-                        println!("got a Foo");
-                        any += 1;
-                    },
-                    BAR_ID => {
-                        let Bar(n) = *downcast::<Bar>(e);
-                        
-                        bar += n;
-                        println!("now {} bar", bar);
-                        send!(1, format!("{} bars", bar));
-                    },
-                    _ => {}
-                }
-            }
-            yield ProcessYield::Empty;
-        }
-    })
-}*/
 fn main() {
     let mut d = Dispatcher::new();
     let printer = d.spawn(|_, inbox| move || loop {
